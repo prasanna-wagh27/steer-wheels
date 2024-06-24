0f2a660d71d0ab78722e0c3cdad051b188b7e74c
Prasanna Wagh <prasannawagh146@gmail.com>
2024-06-25 00:51:00 +0530

    Car module changes

--- src/main/java/com/mycar/steerwheels/controller/CarController.java
+++ src/main/java/com/mycar/steerwheels/controller/CarController.java
@@ -1,9 +1,21 @@
 package com.mycar.steerwheels.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mycar.steerwheels.bo.Response;
+import com.mycar.steerwheels.constants.ErrorConstants;
+import com.mycar.steerwheels.entity.Car;
 import com.mycar.steerwheels.service.CarService;
 
 @RestController
@@ -12,5 +24,24 @@ public class CarController {
 	
 	@Autowired
 	private CarService carService;
+	
+	@CrossOrigin
+	@PostMapping
+	public ResponseEntity<Response> addCar(@RequestBody Car car) throws Exception{
+		carService.addCar(car);
+		Response response = new Response();
+		response.setStatus(ErrorConstants.SUCCESS.toString());
+		response.setMessage("Car added successfully");
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
+	}
 
+	@CrossOrigin
+	@PutMapping("/{carId}")
+	public ResponseEntity<Response> updateCar(@PathVariable("carId") UUID carId, @RequestBody Car car) throws Exception{
+		carService.updateCar(carId, car);
+		Response response = new Response();
+		response.setStatus(ErrorConstants.SUCCESS.toString());
+		response.setMessage("Car updated successfully");
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
+	}
 }
